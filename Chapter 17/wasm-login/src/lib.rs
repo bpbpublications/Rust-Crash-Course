92f285f95a470725d0e354086ed68eb074b29f6a
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2022-07-05 11:19:59 +0530

    By BPB Publications

---
+++ Chapter 17/wasm-login/src/lib.rs
@@ -0,0 +1,29 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn login_failure() {
+    alert("Invalid credentials !!! Try again...");
+}
+
+#[wasm_bindgen]
+pub fn login(uname: &str, pwd: &str) -> bool{
+    if uname == "abhishek" && pwd == "rust" {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
